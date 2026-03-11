d6e2e03179086374c9fb3c91e4556b0b8de51732
Luiso3312MG <rolinomira@gmail.com>
2026-03-10 18:25:28 -0600

    bugFix Impor

--- src/main/java/com/kbseed/controller/ClassController.java
+++ src/main/java/com/kbseed/controller/ClassController.java
@@ -3,6 +3,8 @@ package com.kbseed.controller;
 import com.kbseed.dto.ClassDTO;
 import com.kbseed.service.ClassService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -20,7 +22,7 @@ public class ClassController {
     public List<ClassDTO> obtenerTodas() {
         return classService.obtenerTodas();
     }
-    
+
     @PostMapping
     public ClassDTO crear(@RequestBody ClassDTO dto) {
         return classService.crear(dto);
